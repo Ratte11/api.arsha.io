aafb47c68bccd4ccc787be122565b036efb396c2
Ratte11 <34902133+Ratte11@users.noreply.github.com>
2026-08-09 03:02:51 +0200

    Update RedisConfiguration.java

--- src/main/java/io/arsha/api/config/RedisConfiguration.java
+++ src/main/java/io/arsha/api/config/RedisConfiguration.java
@@ -15,13 +15,9 @@ import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter.EnableKeyspaceEvents;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.PatternTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -121,31 +117,3 @@ public class RedisConfiguration {
 
         return template;
     }
-
-    @Bean
-    RedisMessageListenerContainer container(
-            RedisConnectionFactory connectionFactory,
-            MessageListenerAdapter listenerAdapter
-    ) {
-        RedisMessageListenerContainer container =
-                new RedisMessageListenerContainer();
-
-        container.setConnectionFactory(
-                connectionFactory
-        );
-
-        container.addMessageListener(
-                listenerAdapter,
-                new PatternTopic("__keyevent@*__:expired")
-        );
-
-        return container;
-    }
-
-    @Bean
-    MessageListenerAdapter listenerAdapter(
-            MessageListener listener
-    ) {
-        return new MessageListenerAdapter(listener);
-    }
-}
